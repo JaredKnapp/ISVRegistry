4b93e697b3916eb2f849c672de6969911eda2427
jaredknapp <jared@knapps.net>
2017-08-02 16:44:40 -0400

    Latest cleanup

--- Workspace/app/Model/Platform.php
+++ Workspace/app/Model/Platform.php
@@ -3,7 +3,7 @@ App::uses('AppModel', 'Model');
 class Platform extends AppModel {
 
 	public $useTable = 'platforms';
-
+	
 	public $order = array("Platform.sortorder" => "asc");
 
     public $belongsTo = array(
@@ -13,7 +13,7 @@ class Platform extends AppModel {
             'fields'=>array('id', 'name')
         )
     );
-
+	
 	public $hasMany = array(
 		'PlatformVersions'=>array(
 			'className'=>'PlatformVersion',
@@ -28,38 +28,23 @@ class Platform extends AppModel {
 		}
 		return false;
 	}
-
-    public function getVersion($id){
-		$sql = "SELECT
-            platforms.id as id,
-			platforms.name as name,
-            platforms.organizations_id as organizations_id,
-			platformversions.id as id,
-			platformversions.version as version,
-			platformversions.visibledefault as visible
-		FROM platforms, platformversions
-		WHERE platforms.id=platformversions.platforms_id
-              AND platformversions.id = $id";
-
-		return $this->query($sql);
-	}
-
+		
 	public function view($org = null){
 		$sql = "SELECT
             platforms.id as id,
 			platforms.name as name,
             platforms.organizations_id as organizations_id,
 			platformversions.id as id,
-			platformversions.version as version,
+			platformversions.version as version, 
 			platformversions.visibledefault as visible
-		FROM platforms, platformversions
+		FROM platforms, platformversions 
 		WHERE platforms.id=platformversions.platforms_id";
 
         if( !is_null($org) && $org > '0' ){
             $sql = $sql . "
             AND platforms.organizations_id = ". $org;
         }
-
+		
         $sql = $sql . "
         ORDER BY platforms.sortorder, platformversions.version ";
 
